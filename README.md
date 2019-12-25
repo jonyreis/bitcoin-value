cf8b14fde8dfb0393bc021c664bc8a45e7bc1a11
Jony Reis <jonyreiscardoso@gmail.com>
2019-12-25 10:24:01 -0300

    Code Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Site para ver o valor do Bitcoin
+# Page para ver o valor do Bitcoin
+
+### Usando Fetch API para verificar o valor do Bitcoin em https://blockchain.info/ticker
--- js/fetch-bitcoin.js
+++ js/fetch-bitcoin.js
@@ -2,13 +2,16 @@ export default function initFetchBitcoin() {
   fetch('https://blockchain.info/ticker')
   .then(response => response.json())
   .then(bitcoin => {
-    const btcReais = bitcoin.BRL.sell.toFixed(2)
-    const btcDollar = bitcoin.USD.sell.toFixed(2)
+    const btcReais = bitcoin.BRL.buy.toLocaleString('pt-BR')
+    const btcDollar = bitcoin.USD.buy.toLocaleString('pt-BR')
 
     const valorReais = document.getElementById('btc-reais')
     const valorDollar = document.getElementById('btc-dollar')
 
     valorReais.innerText = `1 BTC = R$ ${btcReais}`
     valorDollar.innerText = `1 BTC = US$ ${btcDollar}`
+  })
+  .catch(function(err){
+    console.error('Não foi possível achar a informação', err)
   })
 }
